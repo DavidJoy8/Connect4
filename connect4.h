e2704242f15b48ecd3dbeee2b232d026d927cfba
David Joy <49874056+DavidJoy8@users.noreply.github.com>
2022-03-27 22:54:41 -0500

    Initial Files from previous work

---
+++ connect4.h
@@ -0,0 +1,39 @@
+#define TOP_ROW 2233928386592ULL
+#define BOTTOM_ROW 69810262081ULL
+#define ALL_SQUARES 4398046511103ULL
+
+#define AT(n) ((unsigned long long int)1<<(n))
+#define COL(n) (63ULL << (6*n))
+//bitmap of the filled squares
+#define FILLED(board) (board.red | board.yellow)
+//bitmap of the non-filled squares
+#define EMPTY(board) (ALL_SQUARES ^ FILLED(board))
+#define CAN_DROP(board, col) (EMPTY(board) & TOP_ROW & COL(col))
+//red goes first
+#define RED_TURN(board) (board.red==0 || __builtin_popcountll(board.red)==__builtin_popcountll(board.yellow))
+#define YELLOW_TURN(board) (board.red!=0 && __builtin_popcountll(board.red)>__builtin_popcountll(board.yellow))
+//the bit to set for the next piece dropped in column n
+#define NEXT_DROP(board, n) ((BOTTOM_ROW & COL(n)) + (FILLED(board) & COL(n)))
+
+//win checking:
+#define TOP3 3909374676536ULL
+#define TOP4 4188615724860ULL
+#define TOP5 4328236249022ULL
+#define BOT3 488671834567ULL
+#define BOT4 1047153931215ULL
+#define BOT5 2164118124511ULL
+
+#define VERT_WIN(pieces) ((TOP3 & (pieces))>>3 & (TOP4 & (pieces))>>2 & (TOP5 & (pieces))>>1 & (pieces))
+#define HORI_WIN(pieces) ((pieces)>>18 & (pieces)>>12 & (pieces) >> 6 & (pieces))
+#define DIAG_A_WIN(pieces) ((TOP3 & (pieces))>>21 & (TOP4 & (pieces))>>14 & (TOP5 & (pieces))>>7 & (pieces))
+#define DIAG_B_WIN(pieces) ((BOT3 & (pieces))>>15 & (BOT4 & (pieces))>>10 & (BOT5 & (pieces))>>5 & (pieces))
+#define IS_WIN(pieces) (VERT_WIN(pieces) || HORI_WIN(pieces) || DIAG_A_WIN(pieces) || DIAG_B_WIN(pieces))
+
+//scoring:
+//If red wins, their score is (22 - number of red pieces).  This encourages the CPU to win quickly and lose slowly.
+//If yellow wins, their score is (number of yellow pieces - 22).  The idea that negative scores represent the second player winning is borrowed from chess
+//These values are used to start the search.  They are outside the range of obtainable scores, which is [-18, 18] (since a win takes a minimum of 4 pieces)
+#define NEG_INFINITY -25
+#define POS_INFINITY 25
+#define RED_SCORE(pieces) 22 - __builtin_popcountll(pieces)
+#define YELLOW_SCORE(pieces) __builtin_popcountll(pieces) - 22
---
+++ main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+#ifndef CONNECT4_H
+#include "connect4.h"
+#define CONNECT4_H
+#endif
+
+struct board {
+	unsigned long long red;
+	unsigned long long yellow;
+} board;
+unsigned long long allMoves[294];
+
+void init_board() {
+	board.red = 0;
+	board.yellow = 0;
+}
+
+#define CHAR(N) (board.red & AT(N) ? 'R' : board.yellow & AT(N) ? 'Y' : '.')
+void print_board() {
+	for (int i=5; i>-1; i--) {
+		printf("%c%c%c%c%c%c%c\n", CHAR(i), CHAR(i+6), CHAR(i+12), CHAR(i+18), CHAR(i+24), CHAR(i+30), CHAR(i+36));
+	}
+	printf("0123456\n\n");
+}
+
+#define PIECE_COUNT(pieces) (pieces ? __builtin_popcountll(pieces) : 0)
+#define MOVE(col) (CAN_DROP(board, col) ? NEXT_DROP(board, col) : 0)
+unsigned long long *gen_moves() {
+	unsigned long long *base = &allMoves[7*(PIECE_COUNT(board.red) + PIECE_COUNT(board.yellow))];
+	base[0] = MOVE(3);	//examine central moves first
+	base[1] = MOVE(2);
+	base[2] = MOVE(4);
+	base[3] = MOVE(1);
+	base[4] = MOVE(5);
+	base[5] = MOVE(0);
+	base[6] = MOVE(6);
+	return base;
+}
+
+#define MAX(a,b) (a > b ? a : b)
+#define MIN(a,b) (a < b ? a : b)
+
+int red_search(int alpha, int beta) {
+	int best = NEG_INFINITY;
+	unsigned long long *moves = gen_moves();
+	for (int i=0; i<7; i++) { //check for winning moves
+		if (moves[i] && IS_WIN(board.red | moves[i]))
+			return RED_SCORE(board.red | moves[i]);
+	}
+	for (int i=0; i<7; i++) {
+		if(moves[i]) {
+			board.red ^= moves[i];
+			best = MAX(best, yellow_search(alpha, beta));
+			if (best >= beta) {	//alphabeta cutoff
+				board.red ^= moves[i];
+				break;
+			}
+			alpha = MAX(alpha, best);
+			board.red ^= moves[i];
+		}
+	}
+	return best;
+}
+int yellow_search(int alpha, int beta) {
+	int best = POS_INFINITY;
+	unsigned long long *moves = gen_moves();
+	for (int i=0; i<7; i++) { //check for winning moves
+		if (moves[i] && IS_WIN(board.yellow | moves[i])) {
+			return YELLOW_SCORE(board.yellow | moves[i]);
+		}
+	}
+	if (PIECE_COUNT(board.yellow)==20)	//if we don't have a winning move and are placing our 21st piece, it's a full board draw
+		return 0;
+	for (int i=0; i<7; i++) {
+		if(moves[i]) {
+			board.yellow ^= moves[i];
+			best = MIN(best, red_search(alpha, beta));
+			if (best <= alpha) {
+				board.yellow ^= moves[i];
+				break;
+			}
+			beta = MIN(beta, best);
+			board.yellow ^= moves[i];
+		}
+	}
+	return best;
+}
+
+int main() {
+	init_board();
+	board.red    = 17571840;
+	board.yellow = 68719480897;
+	print_board();
+	return 0;
+}
